37dd3eafbd56baca1a022d5feb3676a745da9a4e
RomanNikolaienko <114165942+RomanNikolaienko@users.noreply.github.com>
2023-01-28 15:54:00 +0200

    MyQueue
    My own queue implementation

---
+++ MyQueue/MyQueue.java
@@ -0,0 +1,48 @@
+package MyQueue;
+
+import java.util.Arrays;
+
+public class MyQueue<E> {
+    private Object[] queueArray = new Object[5];
+    private int size;
+    private int front = 0;
+    private int rear;
+
+    public int size(){
+        return size;
+    }
+
+    public void add(E element) {
+        if (size == queueArray.length){
+        Object[] newArray = new Object[queueArray.length * 2];
+            for (int i = 0; i < queueArray.length; i++) {
+                newArray[i] = queueArray[i];
+            }
+            queueArray = newArray;
+        }
+            queueArray[rear] = element;
+            rear++;
+            size++;
+        }
+
+        public Object[] clear(){
+        Object[] newArray = new Object[5];
+        size = front = rear = 0;
+        return queueArray = newArray;
+        }
+
+        public Object peek(){
+        return queueArray[0];
+        }
+
+        public Object poll() {
+        Object[] temp = new Object[queueArray.length];
+            for (int i = 0; i < size; i++) {
+                temp[i] = queueArray[i+1];
+            }
+            queueArray = temp;
+            rear--;
+            size--;
+            return queueArray[0];
+        }
+}
---
+++ MyQueue/QueueTest.java
@@ -0,0 +1,35 @@
+package MyQueue;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTest {
+    public static void main(String[] args) {
+        MyQueue<String> myQueue = new MyQueue<String>();
+        myQueue.add("One");
+        myQueue.add("Two");
+        myQueue.add("Three");
+        myQueue.add("Four");
+        myQueue.add("Five");
+        myQueue.add("Six");
+        myQueue.add("Seven");
+        System.out.println("myQueue.peek() = " + myQueue.peek());
+
+        System.out.println("myQueue.size() = " + myQueue.size());
+        System.out.println("myQueue.peek() = " + myQueue.peek());
+        System.out.println("myQueue.size() = " + myQueue.size());
+        System.out.println("myQueue.poll() = " + myQueue.poll());
+        System.out.println("myQueue.poll() = " + myQueue.poll());
+        System.out.println("myQueue.poll() = " + myQueue.poll());
+        System.out.println("myQueue.poll() = " + myQueue.poll());
+        System.out.println("myQueue.poll() = " + myQueue.poll());
+        System.out.println("myQueue.poll() = " + myQueue.poll());
+        System.out.println("myQueue.poll() = " + myQueue.poll());
+        System.out.println("myQueue.peek() = " + myQueue.peek());
+
+        myQueue.add("Six");
+        myQueue.add("Seven");
+
+        System.out.println("myQueue.peek() = " + myQueue.peek());
+        System.out.println("myQueue.size() = " + myQueue.size());
+    }
+}
